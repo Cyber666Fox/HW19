f9bda1ff6ac25cf6288b8ba0168bde57d14e62d2
Cyber666Fox <104504035+Cyber666Fox@users.noreply.github.com>
2022-08-02 20:55:31 +0800

    initial commit
    initial commit

---
+++ service/auth.py
@@ -0,0 +1,52 @@
+from calendar import calendar
+from datetime import datetime
+
+
+from constants import JWT_SECRET,JWT_ALG
+import jwt 
+from service.user import UserService
+
+class AuthServices:
+    def __init__(self, user_service:UserService):
+        self.user_service = user_service
+
+    def generate_tokens(self, username, password, is_refresh = False):
+        user = self.user_service.get_by_username(username)
+
+        if not user:
+            return False
+
+        if not is_refresh:
+            if not self.user_service.campare_password(password, user.password):
+                return False
+        
+        data = {
+            "username": user.username,
+            "role": user.role,
+        }
+
+        min30 = datetime.datetime.utcnow()+datetime.timedalta(minutes = 30)
+        data ["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode (data, JWT_SECRET, algorithm = JWT_ALG)
+
+        day130 = datetime.datetime.utcnow()+datetime.timedalta(days = 130)
+        data ["exp"] = calendar.timegm (day130.timetuple())
+        refresh_token = jwt.encode (data, JWT_SECRET, algorithm = JWT_ALG)
+
+        return {"access_token":access_token, "refresh_token":refresh_token}
+    
+    def approve_refresh_token (self, refresh_token):
+        data = jwt.encode (refresh_token, JWT_SECRET, algorithms = [JWT_ALG])
+        username = data['username']
+        user = self.user_service.get_by_username(username)
+
+        if not user:
+            return False
+        
+        now = calendar.timegm(datetime.datetime.utcnow().timetuple())
+        expired = data['exp']
+        if now > expired:
+            return False
+        
+        return self.generate_tokens (username, user.password, is_refresh=True)
+
